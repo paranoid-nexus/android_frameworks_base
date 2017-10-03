7506afb6e6f0651d6703dec3abe21365b68b3c88
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-03 17:17:37 -0400

    SystemUI: Add double tap to sleep gesture
    Co-authored-by: Adnan Begovic <adnan@cyngn.com>
    Co-authored-by: Altaf-Mahdi <altaf.mahdi@gmail.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Co-authored-by: pppig236 <priv@pppig236.com>
    Co-authored-by: Roman Birg <roman@cyngn.com>
    Co-authored-by: Zhao Wei Liew <zhaoweiliew@gmail.com>
    Change-Id: I7489204e348906dcf6e34fa04f2121974c22ddb9

--- packages/SystemUI/tests/src/com/android/systemui/shade/NotificationPanelViewControllerBaseTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/shade/NotificationPanelViewControllerBaseTest.java
@@ -696,7 +696,7 @@ public class NotificationPanelViewControllerBaseTest extends SysuiTestCase {
                 mStatusBarWindowStateController,
                 mNotificationShadeWindowController,
                 mDozeLog, mDozeParameters, mCommandQueue, mVibratorHelper,
-                mLatencyTracker, mAccessibilityManager, 0, mUpdateMonitor,
+                mLatencyTracker, mPowerManager, mAccessibilityManager, 0, mUpdateMonitor,
                 mMetricsLogger,
                 mShadeLog,
                 mConfigurationController,
@@ -767,7 +767,8 @@ public class NotificationPanelViewControllerBaseTest extends SysuiTestCase {
                 new ResourcesSplitShadeStateController(),
                 mPowerInteractor,
                 mKeyguardClockPositionAlgorithm,
-                mNaturalScrollingSettingObserver);
+                mNaturalScrollingSettingObserver,
+                mContext);
         mNotificationPanelViewController.initDependencies(
                 mCentralSurfaces,
                 null,
