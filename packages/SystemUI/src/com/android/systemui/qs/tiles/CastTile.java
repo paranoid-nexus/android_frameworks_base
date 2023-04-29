b8b7509b48daeb7588cc16a7241185d613801aa3
nift4 <nift4@protonmail.com>
2023-04-29 14:06:25 +0200

    SystemUI: Enable cast tile without Wi-Fi connection
    * Match Settings behaviour
    * Miracast does not need Wi-Fi connection, it's Wi-Fi Direct
    
    Change-Id: I46c1394f35f993875beaaad547c9709a4355a342

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CastTile.java
@@ -90,7 +90,7 @@ public class CastTile extends QSTileImpl<BooleanState> {
     private final TileJavaAdapter mJavaAdapter;
     private final FeatureFlags mFeatureFlags;
     private boolean mCastTransportAllowed;
-    private boolean mHotspotConnected;
+    private boolean mHotspotEnabled;
     private static final String WFD_ENABLE = "persist.debug.wfd.enable";
 
     @Inject
@@ -322,23 +322,23 @@ public class CastTile extends QSTileImpl<BooleanState> {
     }
 
     private boolean canCastToNetwork() {
-        return mCastTransportAllowed || mHotspotConnected;
+        return mCastTransportAllowed || mHotspotEnabled;
     }
 
-    private void setCastTransportAllowed(boolean connected) {
-        if (connected != mCastTransportAllowed) {
-            mCastTransportAllowed = connected;
-            // Hotspot is not connected, so changes here should update
-            if (!mHotspotConnected) {
+    private void setCastTransportAllowed(boolean enabled) {
+        if (enabled != mCastTransportAllowed) {
+            mCastTransportAllowed = enabled;
+            // Hotspot is not enabled, so changes here should update
+            if (!mHotspotEnabled) {
                 refreshState();
             }
         }
     }
 
-    private void setHotspotConnected(boolean connected) {
-        if (connected != mHotspotConnected) {
-            mHotspotConnected = connected;
-            // Wifi is not connected, so changes here should update
+    private void setHotspotEnabled(boolean enabled) {
+        if (enabled != mHotspotEnabled) {
+            mHotspotEnabled = enabled;
+            // Wifi is not enabled, so changes here should update
             if (!mCastTransportAllowed) {
                 refreshState();
             }
@@ -355,17 +355,7 @@ public class CastTile extends QSTileImpl<BooleanState> {
     private final SignalCallback mSignalCallback = new SignalCallback() {
         @Override
         public void setWifiIndicators(@NonNull WifiIndicators indicators) {
-            // statusIcon.visible has the connected status information
-            if(SystemProperties.getBoolean(WFD_ENABLE, false)) {
-                if(indicators.enabled != mCastTransportAllowed) {
-                    mCastTransportAllowed = indicators.enabled;
-                    refreshState();
-                }
-            } else {
-                boolean enabledAndConnected = indicators.enabled
-                        && (indicators.qsIcon != null && indicators.qsIcon.visible);
-                setCastTransportAllowed(enabledAndConnected);
-            }
+            setCastTransportAllowed(indicators.enabled);
         }
     };
 
@@ -373,8 +363,7 @@ public class CastTile extends QSTileImpl<BooleanState> {
             new HotspotController.Callback() {
                 @Override
                 public void onHotspotChanged(boolean enabled, int numDevices) {
-                    boolean enabledAndConnected = enabled && numDevices > 0;
-                    setHotspotConnected(enabledAndConnected);
+                    setHotspotEnabled(enabled);
                 }
             };
 
